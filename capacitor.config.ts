36f0a2aad7d9a63e131920b33983d51d5dc22966
brayangallegos7 <gallegosestrada.07@gmail.com>
2023-10-25 07:59:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'don´t overthink app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
