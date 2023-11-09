41dc03002d7008e0dd8b7787199ce8f646e632ed
brayangallegos7 <gallegosestrada.07@gmail.com>
2023-11-09 14:43:02 -0700

    se agregó mapas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule} from'@angular/fire/compat'
 import { AngularFireAuthModule } from '@angular/fire/compat/auth'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { AngularFireStorageModule } from '@angular/fire/compat/storage'
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
@@ -25,7 +26,8 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
   provideFirestore(() => getFirestore()),
   AngularFireModule.initializeApp(environment.firebase), 
   AngularFireAuthModule,
-  AngularFirestoreModule
+  AngularFirestoreModule,
+  AngularFireStorageModule
   
 ],
   
--- src/app/productos/productos.page.ts
+++ src/app/productos/productos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from '../services/firestore.service';
 import { Producto } from '../models';
+import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -14,18 +15,31 @@ export class ProductosPage implements OnInit {
 
   newProducto: Producto;
 
+  loading : any;
+
   enableNewProducto = false;
+
+  newImage= '';
   
   private path = 'Productos';
 
-  constructor(public firestoreService: FirestoreService) { }
+  constructor(public firestoreService: FirestoreService,
+              public loadingCtrl: LoadingController,
+              public toastController: ToastController,
+              public alertController: AlertController) { }
 
   ngOnInit() {
     this.getProductos();
   }
 
   guardarProducto(){
-    this.firestoreService.createDoc(this.newProducto,this.path,this.newProducto.id);
+    this.showLoading();
+    this.firestoreService.createDoc(this.newProducto,this.path,this.newProducto.id).then(res=>{
+    this.loading.dismiss();
+    this.presentToast('Guardado con exito');
+    }).catch(error => {
+      this.presentToast('No se pudo guardar');
+    });
   }
 
   getProductos(){
@@ -34,9 +48,36 @@ export class ProductosPage implements OnInit {
     } );
   }
 
-  deleteProducto(producto: Producto){
-    this.firestoreService.deleteDoc(this.path,producto.id)
-  }
+  async deleteProducto(producto: Producto){
+      const alert = await this.alertController.create({
+        cssClass: 'normal',
+        header: 'Advertencia',
+        message: '¿Estas seguro de eliminar el producto?',
+        buttons: [{
+          text:'cancelar',
+          role: 'cancel',
+          cssClass: 'normal',
+          handler: (blah) =>{
+            console.log('confirm cancel: blah')
+          } 
+          },{ 
+          text:'Ok',
+          cssClass: 'normal',
+          handler: (blah) =>{
+            console.log('confirm okay');
+            this.firestoreService.deleteDoc(this.path,producto.id).then(res=>{
+            this.alertController.dismiss();
+            this.presentToast('Eliminado con exito');
+          }).catch(error => {
+            this.presentToast('No se pudo eliminar');
+          });
+          }
+
+        }],
+      });
+      await alert.present();
+    }
+  
 
   nuevo(){
     this.enableNewProducto = true;
@@ -48,5 +89,33 @@ export class ProductosPage implements OnInit {
       id: this.firestoreService.getId(),
       fecha: new Date()
     };
+  }
+
+  async showLoading() {
+    this.loading = await this.loadingCtrl.create({
+      message: 'Espere un momento..',
+    });
+
+    this.loading.present();
+  }
+
+  async presentToast(msg:string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      cssClass: 'normal',
+    });
+
+    await toast.present();
+  }
+
+  newImageUpload(event: any){
+    if (event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = ((image) =>{
+        this.newImage = image.target.result as string;
+      });
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 }
